72e9ef8adaf7862dd36fec45c66516f1957dcc61
prashantbhoite123 <pbhoite985@gmail.com>
2024-06-01 11:11:14 +0530

    logout creaditials true

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -13,7 +13,7 @@ const router = express.Router()
 router.post("/register", postRegister)
 router.post("/login", postLogin)
 router.get("/profile", isAuthentication, getProfile)
-router.get("/logout", isAuthentication, logout)
+router.post("/logout", isAuthentication, logout)
 
 router.put("/update/:id", isAuthentication, userUpdate)
 router.delete("/delete/:id", isAuthentication, deleteUser)
